f9982c8a630ec3b3b6d4bc5858ef2e92e60ffed5
InstanceLabs <danielratke97@hotmail.de>
2014-05-28 15:56:10 +0200

    Adds economy

--- API/src/main/java/com/comze_instancelabs/minigamesapi/Classes.java
+++ API/src/main/java/com/comze_instancelabs/minigamesapi/Classes.java
@@ -1,5 +1,7 @@
 package com.comze_instancelabs.minigamesapi;
 
+import net.milkbowl.vault.economy.EconomyResponse;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -18,25 +20,23 @@ public class Classes {
 			public void onOptionClick(IconMenu.OptionClickEvent event) {
 				String d = event.getName();
 				Player p = event.getPlayer();
-				if(MinigamesAPI.getAPI().pinstances.get(plugin).aclasses.containsKey(d)){
+				if (MinigamesAPI.getAPI().pinstances.get(plugin).aclasses.containsKey(d)) {
 					Classes.setClass(plugin, d, p.getName());
 				}
 				event.setWillClose(true);
 			}
 		}, plugin);
-		
+
 		int c = 0;
-		for(String ac : MinigamesAPI.getAPI().pinstances.get(plugin).aclasses.keySet()){
+		for (String ac : MinigamesAPI.getAPI().pinstances.get(plugin).aclasses.keySet()) {
 			iconm.setOption(c, new ItemStack(Material.SLIME_BALL), ac, MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getString("config.kits." + ac + ".lore"));
 			c++;
 		}
-		
+
 		iconm.open(Bukkit.getPlayerExact(p));
 	}
-	
-	
 
-	public static void getClass(JavaPlugin plugin, String player){
+	public static void getClass(JavaPlugin plugin, String player) {
 		AClass c = MinigamesAPI.getAPI().pinstances.get(plugin).pclass.get(player);
 		Player p = Bukkit.getServer().getPlayer(player);
 		p.getInventory().clear();
@@ -48,41 +48,41 @@ public class Classes {
 		p.getInventory().setContents(c.getItems());
 		p.updateInventory();
 	}
-	
-	public static void setClass(JavaPlugin plugin, String classname, String player){
-		if(!kitPlayerHasPermission(plugin, classname, Bukkit.getPlayer(player))){
+
+	public static void setClass(JavaPlugin plugin, String classname, String player) {
+		if (!kitPlayerHasPermission(plugin, classname, Bukkit.getPlayer(player))) {
 			Bukkit.getPlayer(player).sendMessage(MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getString("strings.nokitperm"));
 			return;
 		}
-		if(kitRequiresMoney(plugin, classname)){
+		if (kitRequiresMoney(plugin, classname)) {
 			kitTakeMoney(plugin, Bukkit.getPlayer(player), classname.toLowerCase());
 		}
 		MinigamesAPI.getAPI().pinstances.get(plugin).pclass.put(player, MinigamesAPI.getAPI().pinstances.get(plugin).aclasses.get(classname));
 	}
-	
-	public static void loadClasses(JavaPlugin plugin){
-		if(MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().isSet("config.kits")){
-			for(String aclass : MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getConfigurationSection("config.kits.").getKeys(false)){
+
+	public static void loadClasses(JavaPlugin plugin) {
+		if (MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().isSet("config.kits")) {
+			for (String aclass : MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getConfigurationSection("config.kits.").getKeys(false)) {
 				AClass n = new AClass(plugin, aclass, Util.parseItems(MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getString("config.kits." + aclass + ".items")));
 				MinigamesAPI.getAPI().pinstances.get(plugin).aclasses.put(aclass, n);
-				if(!MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().isSet("config.kits." + aclass + ".items") || !MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().isSet("config.kits." + aclass + ".lore")){
+				if (!MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().isSet("config.kits." + aclass + ".items") || !MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().isSet("config.kits." + aclass + ".lore")) {
 					plugin.getLogger().warning("One of the classes found in the config file is invalid: " + aclass + ". Missing itemid or lore!");
 				}
 			}
 		}
 	}
-	
-	public static boolean kitRequiresMoney(JavaPlugin plugin, String kit){
+
+	public static boolean kitRequiresMoney(JavaPlugin plugin, String kit) {
 		return MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getBoolean("config.kits." + kit + ".requires_money");
 	}
-	
+
 	public static boolean kitTakeMoney(JavaPlugin plugin, Player p, String kit) {
-		if(!economy){
+		if (!MinigamesAPI.getAPI().economy) {
 			plugin.getLogger().warning("Economy is turned OFF. Turn it ON in the config.");
 			return false;
 		}
-		if (econ.getBalance(p.getName()) >= MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getInt("config.kits." + kit + ".money_amount")) {
-			EconomyResponse r = econ.withdrawPlayer(p.getName(), MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getInt("config.kits." + kit + ".money_amount"));
+		if (MinigamesAPI.getAPI().econ.getBalance(p.getName()) >= MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getInt("config.kits." + kit + ".money_amount")) {
+			EconomyResponse r = MinigamesAPI.getAPI().econ.withdrawPlayer(p.getName(), MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getInt("config.kits." + kit + ".money_amount"));
 			if (!r.transactionSuccess()) {
 				p.sendMessage(String.format("An error occured: %s", r.errorMessage));
 			}
@@ -92,19 +92,17 @@ public class Classes {
 			return false;
 		}
 	}
-	
-	public static boolean kitPlayerHasPermission(JavaPlugin plugin, String kit, Player p){
-		if(!MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getBoolean("config.kits." + kit + ".requires_permission")){
+
+	public static boolean kitPlayerHasPermission(JavaPlugin plugin, String kit, Player p) {
+		if (!MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getBoolean("config.kits." + kit + ".requires_permission")) {
 			return true;
-		}else{
-			if(p.hasPermission(MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getString("config.kits." + kit + ".permission_node"))){
+		} else {
+			if (p.hasPermission(MinigamesAPI.getAPI().pinstances.get(plugin).getClassesConfig().getConfig().getString("config.kits." + kit + ".permission_node"))) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 		}
 	}
-	
-	
-	
+
 }
--- API/src/main/java/com/comze_instancelabs/minigamesapi/MinigamesAPI.java
+++ API/src/main/java/com/comze_instancelabs/minigamesapi/MinigamesAPI.java
@@ -3,8 +3,11 @@ package com.comze_instancelabs.minigamesapi;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import net.milkbowl.vault.economy.Economy;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comze_instancelabs.minigamesapi.commands.CommandHandler;
@@ -16,39 +19,46 @@ import com.comze_instancelabs.minigamesapi.util.Util;
 public class MinigamesAPI extends JavaPlugin {
 
 	static MinigamesAPI instance = null;
-	
+	public static Economy econ = null;
+	public boolean economy = true;
+
 	public static HashMap<JavaPlugin, PluginInstance> pinstances = new HashMap<JavaPlugin, PluginInstance>();
-	
+
 	public static HashMap<String, Arena> global_players = new HashMap<String, Arena>();
 	public static HashMap<String, Arena> global_lost = new HashMap<String, Arena>();
 	public static ArrayList<String> global_leftplayers = new ArrayList<String>();
-	
+
 	int lobby_countdown = 30;
 	int ingame_countdown = 10;
 
-	public void onEnable(){
+	public void onEnable() {
 		instance = this;
 
 		String version = Bukkit.getServer().getClass().getPackage().getName().substring(Bukkit.getServer().getClass().getPackage().getName().lastIndexOf(".") + 1);
 		Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Loaded MinigamesAPI. We're on " + version + ".");
 
-		//TODO setup Vault
-		
-		//TODO setup Updater and Metrics
-		
+		if (economy) {
+			if (!setupEconomy()) {
+				getLogger().severe(String.format("[%s] - No iConomy dependency found! Disabling Economy.", getDescription().getName()));
+				economy = false;
+			}
+		}
+
+		// TODO setup Updater and Metrics
+
 	}
-	
-	
-	public void onDisable(){
-		
+
+	public void onDisable() {
+
 	}
-	
+
 	/**
 	 * Sets up the API, stuff won't work without that
+	 * 
 	 * @param plugin_
 	 * @return
 	 */
-	public static MinigamesAPI setupAPI(JavaPlugin plugin_){
+	public static MinigamesAPI setupAPI(JavaPlugin plugin_) {
 		ArenasConfig arenasconfig = new ArenasConfig(plugin_);
 		MessagesConfig messagesconfig = new MessagesConfig(plugin_);
 		ClassesConfig classesconfig = new ClassesConfig(plugin_);
@@ -56,14 +66,25 @@ public class MinigamesAPI extends JavaPlugin {
 		Bukkit.getPluginManager().registerEvents(new ArenaListener(plugin_), plugin_);
 		return instance;
 	}
-	
-	public static MinigamesAPI getAPI(){
+
+	public static MinigamesAPI getAPI() {
 		return instance;
 	}
-	
-	public static CommandHandler getCommandHandler(){
+
+	public static CommandHandler getCommandHandler() {
 		return new CommandHandler();
 	}
 
-	
+	private boolean setupEconomy() {
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		econ = rsp.getProvider();
+		return econ != null;
+	}
+
 }
