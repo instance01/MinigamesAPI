2d398edf92acb1d14de4cf0b4d90582a30e4f785
InstanceLabs <danielratke97@hotmail.de>
2014-08-04 17:37:00 +0200

    Cleanup

--- API/src/main/java/com/comze_instancelabs/minigamesapi/util/AClass.java
+++ API/src/main/java/com/comze_instancelabs/minigamesapi/util/AClass.java
@@ -5,39 +5,42 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 
-
 public class AClass {
 
 	private JavaPlugin plugin;
 	private String name;
 	private ArrayList<ItemStack> items = new ArrayList<ItemStack>();
 	private ItemStack icon;
-	
-	public AClass(JavaPlugin plugin, String name, ArrayList<ItemStack> items){
+
+	public AClass(JavaPlugin plugin, String name, ArrayList<ItemStack> items) {
 		this.plugin = plugin;
 		this.name = name;
 		this.items = items;
 		this.icon = items.get(0);
 	}
-	
-	public AClass(JavaPlugin plugin, String name, ArrayList<ItemStack> items, ItemStack icon){
+
+	public AClass(JavaPlugin plugin, String name, ArrayList<ItemStack> items, ItemStack icon) {
 		this.plugin = plugin;
 		this.name = name;
 		this.items = items;
 		this.icon = icon;
 	}
-	
-	public ItemStack[] getItems(){
+
+	public ItemStack[] getItems() {
 		ItemStack[] ret = new ItemStack[items.size()];
 		int c = 0;
-		for(ItemStack f : items){
+		for (ItemStack f : items) {
 			ret[c] = f;
 			c++;
 		}
 		return ret;
 	}
-	
-	public ItemStack getIcon(){
+
+	public ItemStack getIcon() {
 		return this.icon;
+	}
+
+	public String getName() {
+		return this.name;
 	}
 }
