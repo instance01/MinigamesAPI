4878803da5798ce5535c5ed767a97a42c0dd0f6f
mepeisen <mep_eisen@web.de>
2016-09-13 18:35:36 +0200

    v2

--- APIv2/src/main/java/com/github/mce/minigames/api/arena/rules/bevents/MinigameCreatureSpawnEvent.java
+++ APIv2/src/main/java/com/github/mce/minigames/api/arena/rules/bevents/MinigameCreatureSpawnEvent.java
@@ -22,6 +22,17 @@ import com.github.mce.minigames.api.arena.rules.MinigameEvent;
 /**
  * Minigame event representation of corresponding bukkit event.
  * 
+ * <h3>Using this event as a rule</h3>
+ * 
+ * <dl>
+ * <dt>Player-Rule</dt>
+ * <dd>not supprted.</dd>
+ * <dt>Arena-Rule</dt>
+ * <dd>Passed to the arena calculated from spawn location</dd>
+ * <dt>Minigame-Rule</dt>
+ * <dd>Passed to the minigame calculated from arena</dd>
+ * </dl>
+ * 
  * @author mepeisen
  */
 public interface MinigameCreatureSpawnEvent extends MinigameEvent<CreatureSpawnEvent, MinigameCreatureSpawnEvent>
--- APIv2/src/main/java/com/github/mce/minigames/api/arena/rules/bevents/MinigameEnderDragonChangePhaseEvent.java
+++ APIv2/src/main/java/com/github/mce/minigames/api/arena/rules/bevents/MinigameEnderDragonChangePhaseEvent.java
@@ -23,6 +23,17 @@ import com.github.mce.minigames.api.arena.rules.MinigameEvent;
 /**
  * Minigame event representation of corresponding bukkit event.
  * 
+ * <h3>Using this event as a rule</h3>
+ * 
+ * <dl>
+ * <dt>Player-Rule</dt>
+ * <dd>not supported.</dd>
+ * <dt>Arena-Rule</dt>
+ * <dd>Passed to the arena calculated from ender dragon location</dd>
+ * <dt>Minigame-Rule</dt>
+ * <dd>Passed to the minigame calculated from arena</dd>
+ * </dl>
+ * 
  * @author mepeisen
  */
 @MinecraftVersionRange(min = MinecraftVersionsType.V1_9_R1)
